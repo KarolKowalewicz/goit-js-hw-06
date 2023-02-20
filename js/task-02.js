e0af28d307e362bd0020af54dd176a80f0819a8b
KarolKowalewicz <kowalewiczkarol@gmail.com>
2023-02-20 14:13:01 +0100

    exercise 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// pobranie listy ul#ingredients
+const ingredientsList = document.querySelector("ul#ingredients");
+
+// utworzenie tablicy elementów <li> i dodanie ich do listy
+const ingredientsItems = ingredients.map((ingredient) => {
+  // utworzenie nowego elementu <li>
+  const item = document.createElement("li");
+  // ustawienie zawartości tekstowej na nazwę składnika
+  item.textContent = ingredient;
+  // dodanie klasy "item"
+  item.classList.add("item");
+  return item;
+});
+
+// dodanie wszystkich elementów <li> do listy ul#ingredients
+ingredientsList.append(...ingredientsItems);
